ee87a5bb02a4f183e33851c34bcab3ebebc1557f
xufei <xufei@hlchange.cn>
2019-05-15 13:58:58 +0800

    Remove ch16

--- src/ch16/women/expample1/IHandler.java
+++
@@ -1,5 +0,0 @@
-package ch16.women.expample1;
-
-public interface IHandler {
-    public void HandleMessage(IWomen women);
-}
--- src/ch16/women/expample1/IWomen.java
+++
@@ -1,7 +0,0 @@
-package ch16.women.expample1;
-
-public interface IWomen {
-    public int getType();
-
-    public String getRequest();
-}
